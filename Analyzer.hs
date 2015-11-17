bb84bd29beece72408913956c6770b66df1b82da
Lukas Martinelli <me@lukasmartinelli.ch>
2015-11-17 18:14:45 +0100

    Check for maintainer and remove empty checks

--- Analyzer.hs
+++ Analyzer.hs
@@ -15,19 +15,30 @@ category :: Rule -> Category
 category ExplicitTag        = BestPractice
 category ExplicitMaintainer = BestPractice
 
+hasMaintainer :: Instruction -> Bool
+hasMaintainer (Maintainer _) = True
+hasMaintainer _              = False
+
+explicitMaintainer :: Dockerfile -> Check
+explicitMaintainer dockerfile =
+    if or $ map hasMaintainer dockerfile
+        then Success ExplicitMaintainer
+        else Failed ExplicitMaintainer
+
 explicitTag :: BaseImage -> Check
 explicitTag (LatestImage img)           = Success ExplicitTag
 explicitTag (TaggedImage img tag)       = Failed ExplicitTag
 explicitTag (DigestedImage img digest)  = Failed ExplicitTag
 
-checkInstruction :: Instruction -> Maybe Check
-checkInstruction (From instr) = Just $ explicitTag instr
-checkInstruction _            = Nothing
+checkInstruction :: Instruction -> [Check]
+checkInstruction (From instr) = [explicitTag instr]
+checkInstruction _            = []
 
--- Run checks that apply to the entire AST tree and not on a single nod
-checkDockerfile :: Dockerfile -> [Maybe Check]
-checkDockerfile dockerfile = map checkInstruction dockerfile
+checkDockerfile :: Dockerfile -> [Check]
+checkDockerfile dockerfile = nodeChecks ++ treeChecks
+    where nodeChecks = concat $ map checkInstruction dockerfile
+          treeChecks = [explicitMaintainer dockerfile]
 
-analyze :: Either t Dockerfile -> Maybe [Maybe Check]
+analyze :: Either t Dockerfile -> Maybe [Check]
 analyze (Left err) = Nothing
 analyze (Right d)  = Just $ checkDockerfile d
