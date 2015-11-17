8611fd4321f3705771f10db3c4b002cb053f901d
Lukas Martinelli <me@lukasmartinelli.ch>
2015-11-17 19:11:05 +0100

    Add more checks and simplify

--- Analyzer.hs
+++ Analyzer.hs
@@ -5,6 +5,20 @@ import Syntax
 data Rule
   = ExplicitTag
   | ExplicitMaintainer
+  | SortMultilineArguments
+  | MaximumNumberLayers
+  | AvoidPackages
+  | OneProcess
+  | BuildCache
+  | OfficialBaseImage
+  | AptGetVersionPinning
+  | AptGetCleanup
+  | CmdUseExecForm
+  | ChainEnv
+  | NoRootUser
+  | NoSudo
+  | UseWorkdir
+  | AbsoluteWorkdir
   deriving(Show)
 
 
@@ -12,8 +26,17 @@ data Check = Success Rule | Failed Rule deriving(Show)
 data Category = Error | BestPractice deriving(Show)
 
 category :: Rule -> Category
-category ExplicitTag        = BestPractice
-category ExplicitMaintainer = BestPractice
+category MaximumNumberLayers    = Error
+category NoSudo                 = Error
+category ExplicitTag            = BestPractice
+category ExplicitMaintainer     = BestPractice
+category SortMultilineArguments = BestPractice
+category AbsoluteWorkdir        = BestPractice
+category UseWorkdir             = BestPractice
+
+-- take boolean value and turn it into a failed or successful check
+asCheck t True  = Success t
+asCheck t False = Failed t
 
 hasMaintainer :: Instruction -> Bool
 hasMaintainer (Maintainer _) = True
@@ -25,14 +48,28 @@ explicitMaintainer dockerfile =
         then Success ExplicitMaintainer
         else Failed ExplicitMaintainer
 
-explicitTag :: BaseImage -> Check
-explicitTag (LatestImage img)           = Success ExplicitTag
-explicitTag (TaggedImage img tag)       = Failed ExplicitTag
-explicitTag (DigestedImage img digest)  = Failed ExplicitTag
+-- check if base image has a explicit tag
+explicitTag :: BaseImage -> Bool
+explicitTag (LatestImage img)    = False
+explicitTag (TaggedImage _ _)    = True
+explicitTag (DigestedImage _ _)  = True
+
+usingSudo :: Arguments -> Bool
+usingSudo args = elem "sudo" args
+
+-- check if cd occurs in run arguments
+usingWorkdir :: Arguments -> Bool
+usingWorkdir args = elem "cd" args
+
+-- check if directory path is absolute
+absoluteWorkdir :: Directory -> Bool
+absoluteWorkdir dir = (head dir) == '/'
 
 checkInstruction :: Instruction -> [Check]
-checkInstruction (From instr) = [explicitTag instr]
-checkInstruction _            = []
+checkInstruction (From instr)  = [asCheck ExplicitTag $ explicitTag instr]
+checkInstruction (Workdir dir) = [asCheck AbsoluteWorkdir $ absoluteWorkdir dir]
+checkInstruction (Run args)    = [asCheck UseWorkdir $ usingWorkdir args]
+checkInstruction _             = []
 
 checkDockerfile :: Dockerfile -> [Check]
 checkDockerfile dockerfile = nodeChecks ++ treeChecks
