7aa7eae12a26df7151d5d2be482b22665e7430fb
Lukas Martinelli <me@lukasmartinelli.ch>
2016-01-04 22:13:09 +0100

    Try build JavaScript API with shellcheck support

--- src/JavaScriptApi.hs
+++ src/JavaScriptApi.hs
@@ -2,26 +2,24 @@
 module Main where
 
 import Parser
-import Analyzer
 import Formatter
 import Rules
 import Syntax
+import Bash
 
 import Haste
 import Haste.Foreign
 
 instance ToAny Check where
-    toAny (DockerfileCheck rule _)    = toObject [("message", toAny (message rule))
-                                                 ,("name", toAny (name rule))]
-    toAny (InstructionCheck rule pos) = toObject [("message", toAny (message rule))
-                                                 ,("name", toAny (name rule))
-                                                 ,("linenumber", toAny pos)]
+    toAny (rule, result) = toObject [("message", toAny (message rule))
+                                    ,("name", toAny (name rule))
+                                    ,("linenumber", toAny result)]
 
-instance ToAny InstructionPos where
-    toAny (InstructionPos _ linenumber) = toAny linenumber
+instance ToAny RuleResult where
+    toAny (linenumber, _) = toAny linenumber
 
 analyzeString :: String -> IO [Check]
-analyzeString str = return $ failedChecks checks
+analyzeString str = return checks
     where ast = parseString str
           analyzeEither (Left err) = []
           analyzeEither (Right d) = analyze d
