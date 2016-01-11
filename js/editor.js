6bf31e5f37437ec4395464d0aac961331601809b
Lukas Martinelli <me@lukasmartinelli.ch>
2016-01-11 13:51:46 +0100

    Use different example for Dockerfile

--- js/editor.js
+++ js/editor.js
@@ -14,9 +14,14 @@ $(function() {
 
         widgets = [];
         $.each(checks, function(_, check) {
-          var inlineText = check.metadata.code + ': ' + check.metadata.message;
-          var hint = $('<div class="lint-error"></div>').text(inlineText).get(0);
-          var widget = editor.addLineWidget(check.linenumber - 1, hint, {
+          var msg = $('<span class="lint-message"></span>').text(check.metadata.message);
+          var inline = $('<a class="lint-link" href="' + check.link + '">' + check.metadata.code + ' </a>').append(msg);
+          var hint = $('<div class="lint-error"></div>').html(inline).get(0);
+          var linenumber = check.linenumber - 1;
+          if (linenumber < 0) {
+                linenumber = 0;
+          }
+          var widget = editor.addLineWidget(linenumber, hint, {
             coverGutter: false,
             noHScroll: true,
             above: true
@@ -25,9 +30,11 @@ $(function() {
         });
       });
     }
+
     $('#clear').click(function() {
         updateHints([]);
     });
+
     $('#lint').click(function() {
       var src = editor.getDoc().getValue() + '\n';
       $.post('http://api.hadolint.lukasmartinelli.ch/dockerfile', { dockerfile: src }, function(checks) {
