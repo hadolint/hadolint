6131cabb9efbb05eebb3fefa7be456be0c8de7c7
Lukas Martinelli <me@lukasmartinelli.ch>
2016-01-11 10:33:50 +0100

    Switch to hadolint API

--- js/editor.js
+++ js/editor.js
@@ -1,41 +1,42 @@
 $(function() {
-var editor = CodeMirror.fromTextArea(document.getElementById('dockerfile__code'), {
-  mode: 'text/x-dockerfile',
-  lineNumbers: true,
-  viewportMargin: Infinity,
-});
-var widgets = [];
-
-function updateHints(checks) {
-  editor.operation(function() {
-    $.each(widgets, function(_, widget) {
-      editor.removeLineWidget(widget);
+    var editor = CodeMirror.fromTextArea(document.getElementById('dockerfile__code'), {
+      mode: 'text/x-dockerfile',
+      lineNumbers: true,
+      viewportMargin: Infinity,
     });
+    var widgets = [];
 
-    widgets = [];
-    $.each(checks, function(_, check) {
-      var hint = $('<div class="lint-error">' + check.message +'</div>').get(0);
-      var linenumber = 0;
-      if(check.linenumber) {
-        linenumber = check.linenumber - 1;
-      }
-      var widget = editor.addLineWidget(linenumber, hint, {
-        coverGutter: false,
-        noHScroll: true,
-        above: true
+    function updateHints(checks) {
+      editor.operation(function() {
+        $.each(widgets, function(_, widget) {
+          editor.removeLineWidget(widget);
+        });
+
+        widgets = [];
+        $.each(checks, function(_, check) {
+          var hint = $('<div class="lint-error"></div>').text(check.message).get(0);
+          var linenumber = 0;
+          if(check.linenumber) {
+            linenumber = check.linenumber - 1;
+          }
+          var widget = editor.addLineWidget(linenumber, hint, {
+            coverGutter: false,
+            noHScroll: true,
+            above: true
+          });
+          widgets.push(widget);
+        });
       });
-      widgets.push(widget);
+    }
+    $('#lint').click(function() {
+      var src = editor.getDoc().getValue();
+      $.post('http://hadolint.apps.lukasmartinelli.ch/dockerfile', { dockerfile: src }, function(checks) {
+          console.log(checks);
+          updateHints(checks);
+      });
     });
-  });
-}
-$('#lint').click(function() {
-  var src = editor.getDoc().getValue();
-  var checks = Haste.analyzeString(src);
-  console.log(checks);
-  updateHints(checks);
-});
 
-window.setTimeout(function() {
-  $('#lint').click();
-}, 2000);
+    window.setTimeout(function() {
+      $('#lint').click();
+    }, 1000);
 });
