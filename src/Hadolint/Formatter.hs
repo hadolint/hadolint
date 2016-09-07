f437c7ae4e2e79916b5cc3e1b40cfe4a99e27c35
lukasmartinelli <me@lukasmartinelli.ch>
2016-09-07 08:43:57 +0200

    Unified error formatting #57

--- src/Hadolint/Formatter.hs
+++ src/Hadolint/Formatter.hs
@@ -1,8 +1,20 @@
-module Hadolint.Formatter (formatCheck) where
+module Hadolint.Formatter (formatCheck, formatError) where
 
+import Data.Char (isSpace)
 import Control.Monad
 import Hadolint.Rules
 import Hadolint.Syntax
+import Text.Parsec.Error (Message, ParseError, messageString, errorPos, errorMessages, showErrorMessages)
+import Text.Parsec.Pos
+
+formatError :: ParseError -> String
+formatError err =  posPart ++ stripNewlines msgPart
+  where
+    pos = errorPos err
+    posPart = sourceName pos ++ ":" ++ show (sourceLine pos) ++ ":" ++ show (sourceColumn pos)
+    msgPart = showErrorMessages "or" "unknown parse error" "expecting" "unexpected" "end of input" (errorMessages err)
+    stripNewlines = map (\c -> if c =='\n' then ' ' else c)
+
 
 formatCheck :: Check -> String
 formatCheck (Check metadata source linenumber _) = formatPos source linenumber ++ code metadata ++ " " ++ message metadata
