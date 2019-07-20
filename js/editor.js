cfda0871057d362d6b96854df65ac63ae6ee44b0
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2019-07-20 17:38:14 +0200

    Updating page to use pure javascript implementation

--- js/editor.js
+++ js/editor.js
@@ -1,45 +1,70 @@
-$(function() {
-    var editor = CodeMirror.fromTextArea(document.getElementById('dockerfile__code'), {
-      mode: 'text/x-dockerfile',
-      lineNumbers: true,
-      viewportMargin: Infinity,
-    });
-    var widgets = [];
+(function() {
+  window.editor = CodeMirror.fromTextArea(document.getElementById('dockerfile__code'), {
+    mode: 'text/x-dockerfile',
+    lineNumbers: true,
+    viewportMargin: Infinity,
+  });
 
-    function updateHints(checks) {
-      editor.operation(function() {
-        $.each(widgets, function(_, widget) {
-          editor.removeLineWidget(widget);
-        });
+  document.getElementById('clear').onclick = function () {
+    updateHints([]);
+  }
+})();
 
-        widgets = [];
-        $.each(checks, function(_, check) {
-          var msg = $('<span class="lint-message"></span>').text(check.metadata.message);
-          var inline = $('<a class="lint-link" href="' + check.link + '">' + check.metadata.code + ' </a>').append(msg);
-          var hint = $('<div class="lint-error"></div>').html(inline).get(0);
-          var linenumber = check.linenumber - 1;
-          if (linenumber < 0) {
-                linenumber = 0;
-          }
-          var widget = editor.addLineWidget(linenumber, hint, {
-            coverGutter: false,
-            noHScroll: true,
-            above: true
-          });
-          widgets.push(widget);
-        });
-      });
+let widgets = []
+let lintButton = document.getElementById('lint')
+
+function showLoading() {
+  let loading = document.getElementById('loading')
+  loading.style.display = 'inline';
+}
+
+function hideLoading() {
+  let loading = document.getElementById('loading')
+  loading.style.display = 'none';
+}
+
+function updateHints(checks) {
+  editor.operation(function () {
+    for (var i = 0; i < widgets.length; i++) {
+      editor.removeLineWidget(widgets[i]);
     }
 
-    $('#clear').click(function() {
-        updateHints([]);
-    });
+    widgets = [];
+    for (var i = 0; i < checks.length; i++) {
+      var check = checks[i];
 
-    $('#lint').click(function() {
-      var src = editor.getDoc().getValue() + '\n';
-      $.post('http://api.hadolint.lukasmartinelli.ch/dockerfile', { dockerfile: src }, function(checks) {
-          console.log(checks);
-          updateHints(checks);
-      });
-    });
-});
+      var msg = document.createElement("span");
+      msg.className = "lint-message";
+      msg.innerText = check.message;
+
+      if (check.code != 'DL1000') {
+        var link = 'https://github.com/hadolint/hadolint/wiki/'
+        if (check.code.startsWith('SC')) {
+          link = 'https://github.com/koalaman/shellcheck/wiki/';
+        }
+
+        var inline = document.createElement('a')
+        inline.href = link + check.code;
+        inline.className = "lint-link";
+        inline.append(msg);
+      } else {
+        var inline = msg;
+      }
+
+      var hint = document.createElement('div');
+      hint.className = "lint-error";
+      hint.append(inline);
+
+      var linenumber = check.line - 1;
+      if (linenumber < 0) {
+        linenumber = 0;
+      }
+
+      widgets.push(editor.addLineWidget(linenumber, hint, {
+        coverGutter: false,
+        noHScroll: true,
+        above: true
+      }));
+    }
+  });
+}
