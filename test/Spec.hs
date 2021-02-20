80b7b7fed108f66da1944f1dd747ac502297ef36
Moritz Röhrich <moritz@ildefons.de>
2021-02-20 13:38:02 +0100

    bump: language-docker v9.1.3, hadolint 1.22.2
    - Bump version of language-docker to 9.1.3
    - Bump version of Hadolint to 1.22.2
    - Add regression test for #510
    
    fixes #510

--- test/Spec.hs
+++ test/Spec.hs
@@ -1422,7 +1422,7 @@ main =
       it "ok with `FROM` outside of `ONBUILD`" $ ruleCatchesNot noIllegalInstructionInOnbuild "FROM debian:buster"
       it "ok with `MAINTAINER` outside of `ONBUILD`" $ ruleCatchesNot noIllegalInstructionInOnbuild "MAINTAINER \"Some Guy\""
     --
-    describe "Regression Tests" $
+    describe "Regression Tests" $ do
       it "Comments with backslashes at the end are just comments" $
         let dockerFile =
               [ "FROM alpine:3.6",
@@ -1434,6 +1434,16 @@ main =
                 "RUN echo \"kaka\" | sed 's/a/o/g' >> /root/afile"
               ]
          in ruleCatches usePipefail $ Text.unlines dockerFile
+      it "`ARG` can correctly unset variables" $
+        let dockerFile =
+              [ "ARG A_WITHOUT_EQ",
+                "ARG A_WITH_EQ=",
+                "RUN echo bla"
+              ]
+         in assertChecks
+              shellcheck
+              (Text.unlines dockerFile)
+              (assertBool "No Warnings or Errors should be triggered" . null)
 
     -- Run tests for the Config module
     ConfigSpec.tests
