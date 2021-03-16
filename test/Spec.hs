5c5a644ecd2d79ca97412c5eca16298c0f14d7d0
Michał Sochoń <kaszpir@gmail.com>
2021-03-16 11:11:07 +0100

    Add suggestion to run wget with --progress=dot:giga (#566)
    Fixes #546
    
    Warn when runnign wget without without --progress flag unless
    other flags are present (such as quiet/no-verbose/output/append),
    which usually leads to insane amouts of useless logs when downloaded
    files are big (usually anything over 5MB becomes annoying)
    
    Adding --progress=dot:giga makes output much shorter even for very large
    files such as AM/ML models etc.
    
    See wget manual, search for --progress=dot:giga
    
    Signed-off-by: Michał Sochoń <kaszpir@gmail.com>

--- test/Spec.hs
+++ test/Spec.hs
@@ -1507,6 +1507,68 @@ main =
               (Text.unlines dockerFile)
               (failsWith 2 "DL4001")
     --
+    describe "Wget no progress" $ do
+      it "warns when using wget without --progress option" $
+        let dockerFile =
+              [ "FROM node as foo",
+                "RUN wget my.xyz"
+              ]
+         in ruleCatches "DL3047" $ Text.unlines dockerFile
+      it "does not warn when running with --progress option" $
+        let dockerFile =
+              [ "FROM node as foo",
+                "RUN wget --progress=dot:giga my.xyz"
+              ]
+         in ruleCatchesNot "DL3047" $ Text.unlines dockerFile
+      it "does not warn when running with -q (quiet short option) and without --progress option" $
+        let dockerFile =
+              [ "FROM node as foo",
+                "RUN wget -q my.xyz"
+              ]
+         in ruleCatchesNot "DL3047" $ Text.unlines dockerFile
+      it "does not warn when running with --quiet (quiet long option) and without --progress option" $
+        let dockerFile =
+              [ "FROM node as foo",
+                "RUN wget --quiet my.xyz"
+              ]
+         in ruleCatchesNot "DL3047" $ Text.unlines dockerFile
+      it "does not warn when running with -nv (no-verbose short option) and without --progress option" $
+        let dockerFile =
+              [ "FROM node as foo",
+                "RUN wget -nv my.xyz"
+              ]
+         in ruleCatchesNot "DL3047" $ Text.unlines dockerFile
+      it "does not warn when running with --no-verbose (no-verbose long option) and without --progress option" $
+        let dockerFile =
+              [ "FROM node as foo",
+                "RUN wget --no-verbose my.xyz"
+              ]
+         in ruleCatchesNot "DL3047" $ Text.unlines dockerFile
+      it "does not warn when running with --output-file (output-file long option) and without --progress option" $
+        let dockerFile =
+              [ "FROM node as foo",
+                "RUN wget --output-file=/tmp/wget.log my.xyz"
+              ]
+         in ruleCatchesNot "DL3047" $ Text.unlines dockerFile
+      it "does not warn when running with -o (output-file long option) and without --progress option" $
+        let dockerFile =
+              [ "FROM node as foo",
+                "RUN wget -o /tmp/wget.log my.xyz"
+              ]
+         in ruleCatchesNot "DL3047" $ Text.unlines dockerFile
+      it "does not warn when running with --append-output (append-output long option) and without --progress option" $
+        let dockerFile =
+              [ "FROM node as foo",
+                "RUN wget --append-output=/tmp/wget.log my.xyz"
+              ]
+         in ruleCatchesNot "DL3047" $ Text.unlines dockerFile
+      it "does not warn when running with -a (append-output long option) and without --progress option" $
+        let dockerFile =
+              [ "FROM node as foo",
+                "RUN wget -a /tmp/wget.log my.xyz"
+              ]
+         in ruleCatchesNot "DL3047" $ Text.unlines dockerFile
+    --
     describe "ONBUILD" $ do
       it "error when using `ONBUILD` within `ONBUILD`" $
         let dockerFile =
