111158d0e4d4b4304f17cb32a8c3d1d62581e3da
Viktor Oreshkin <solteke.st@gmail.com>
2021-04-09 17:29:31 +0300

    DL3005: allow apt-get upgrade (fix #562) (#596)

--- test/DL3005.hs
+++ test/DL3005.hs
@@ -8,9 +8,6 @@ tests :: SpecWith ()
 tests = do
   let ?rulesConfig = mempty
   describe "DL3005" $ do
-    it "apt-get upgrade" $ do
-      ruleCatches "DL3005" "RUN apt-get update && apt-get upgrade"
-      onBuildRuleCatches "DL3005" "RUN apt-get update && apt-get upgrade"
     it "apt-get dist-upgrade" $ do
       ruleCatches "DL3005" "RUN apt-get update && apt-get dist-upgrade"
       onBuildRuleCatches "DL3005" "RUN apt-get update && apt-get dist-upgrade"
