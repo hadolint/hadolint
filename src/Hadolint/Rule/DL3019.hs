6a925c3b402ceb49cd346df6b8af52f27c6fe851
Moritz Röhrich <moritz@ildefons.de>
2021-08-01 13:46:36 +0200

    Feature: Silence DL3009,DL3019 for cache mounts (#672)
    In case the cache directories DL3009 and DL3019 respectively are
    provided via BuildKit cache mount, the two rules need not warn a user to
    remove the directories.
    
    fixes: #671

--- src/Hadolint/Rule/DL3019.hs
+++ src/Hadolint/Rule/DL3019.hs
@@ -1,5 +1,6 @@
 module Hadolint.Rule.DL3019 (rule) where
 
+import qualified Data.Text as Text
 import Hadolint.Rule
 import Hadolint.Shell (ParsedShell)
 import qualified Hadolint.Shell as Shell
@@ -11,9 +12,20 @@ rule = simpleRule code severity message check
     code = "DL3019"
     severity = DLInfoC
     message =
-      "Use the `--no-cache` switch to avoid the need to use `--update` and remove \
-      \`/var/cache/apk/*` when done installing packages"
-    check (Run (RunArgs args _)) = foldArguments (Shell.noCommands forgotCacheOption) args
+      "Use the `--no-cache` switch to avoid the need to use `--update` and \
+      \remove `/var/cache/apk/*` when done installing packages"
+    check (Run (RunArgs args flags)) = hasCacheMount flags
+      || foldArguments (Shell.noCommands forgotCacheOption) args
     check _ = True
-    forgotCacheOption cmd = Shell.cmdHasArgs "apk" ["add"] cmd && not (Shell.hasFlag "no-cache" cmd)
 {-# INLINEABLE rule #-}
+
+hasCacheMount :: RunFlags -> Bool
+hasCacheMount RunFlags
+  { mount =
+      Just (CacheMount CacheOpts {cTarget = TargetPath {unTargetPath = p}})
+  } = Text.dropWhileEnd (=='/') p == "/var/cache/apk"
+hasCacheMount RunFlags {} = False
+
+forgotCacheOption :: Shell.Command -> Bool
+forgotCacheOption cmd = Shell.cmdHasArgs "apk" ["add"] cmd
+  && not (Shell.hasFlag "no-cache" cmd)
