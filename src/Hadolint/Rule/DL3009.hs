c931c0a9689cd6dff4d2083fa002414c1f08a586
Moritz Röhrich <moritz@ildefons.de>
2021-10-26 14:15:47 +0200

    DL3009: Fix handling cache mounts (#727)
    - Upgrade language-docker to 10.3.0
    - Deal with multiple `--mount` flags for `RUN` instructions
    
    fixes: #723

--- src/Hadolint/Rule/DL3009.hs
+++ src/Hadolint/Rule/DL3009.hs
@@ -1,19 +1,23 @@
 module Hadolint.Rule.DL3009 (rule) where
 
-import qualified Data.Map.Strict as Map
-import qualified Data.Text as Text
 import Hadolint.Rule
-import qualified Hadolint.Shell as Shell
 import Language.Docker.Syntax
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
+import qualified Data.Text as Text
+import qualified Hadolint.Shell as Shell
+
 
 data Acc
   = Acc
       { lastFrom :: BaseImage,
+        dockerClean :: Bool,
         stages :: Map.Map Text.Text Linenumber,
         forgets :: Map.Map Linenumber BaseImage
       }
   | Empty
-  deriving (Show)
+  deriving (Eq, Show)
+
 
 rule :: Rule Shell.ParsedShell
 rule = veryCustomRule check (emptyState Empty) markFailures
@@ -24,9 +28,14 @@ rule = veryCustomRule check (emptyState Empty) markFailures
 
     check line st (From from) = st |> modify (rememberStage line from)
     check line st (Run (RunArgs args flags))
-      | hasNoCacheMount flags && foldArguments forgotToCleanup args =
-          st |> modify (rememberLine line)
-      | otherwise = st
+      | not (foldArguments forgotToCleanup args) =
+          if foldArguments disabledDockerClean args
+          then st |> modify rememberDockerClean
+          else st
+      | hasCacheDirectory "/var/lib/apt/lists" flags = st
+      | hasCacheDirectory "/var/lib/apt" flags
+        && hasCacheDirectory "/var/cache/apt" flags = st
+      | otherwise = st |> modify (rememberLine line)
     check _ st _ = st
 
     -- Convert the final state into failures
@@ -43,29 +52,53 @@ rule = veryCustomRule check (emptyState Empty) markFailures
 {-# INLINEABLE rule #-}
 
 rememberStage :: Linenumber -> BaseImage -> Acc -> Acc
-rememberStage line from@BaseImage {image = Image _ als} (Acc _ stages o) = Acc from (Map.insert als line stages) o
-rememberStage line from@BaseImage {image = Image _ als} Empty = Acc from (Map.singleton als line) Map.empty
+rememberStage line from@BaseImage {image = Image _ als} (Acc _ _ stages o) = Acc from True (Map.insert als line stages) o
+rememberStage line from@BaseImage {image = Image _ als} Empty = Acc from True (Map.singleton als line) Map.empty
 
 rememberLine :: Linenumber -> Acc -> Acc
-rememberLine line (Acc from stages o) = Acc from stages (Map.insert line from o)
-rememberLine line Empty = Acc emptyImage mempty (Map.singleton line emptyImage)
+rememberLine line (Acc from clean stages o) = Acc from clean stages (Map.insert line from o)
+rememberLine line Empty = Acc emptyImage True mempty (Map.singleton line emptyImage)
+
+rememberDockerClean :: Acc -> Acc
+rememberDockerClean Empty = Acc emptyImage False mempty mempty
+rememberDockerClean (Acc from _ stages forget) = Acc from False stages forget
 
 forgotToCleanup :: Shell.ParsedShell -> Bool
 forgotToCleanup args
-  | hasUpdate && not hasCleanup = True
+  | hasUpdate args && not hasCleanup = True
   | otherwise = False
   where
     hasCleanup =
       any (Shell.cmdHasArgs "rm" ["-rf", "/var/lib/apt/lists/*"]) (Shell.presentCommands args)
 
-    hasUpdate = any (Shell.cmdHasArgs "apt-get" ["update"]) (Shell.presentCommands args)
+hasUpdate :: Shell.ParsedShell -> Bool
+hasUpdate args =
+  any (Shell.cmdHasArgs "apt-get" ["update"]) (Shell.presentCommands args)
 
-hasNoCacheMount :: RunFlags -> Bool
-hasNoCacheMount RunFlags
-  { mount =
-      Just (CacheMount CacheOpts {cTarget = TargetPath {unTargetPath = p}})
-  } = Text.dropWhileEnd (=='/') p /= "/var/lib/apt/lists"
-hasNoCacheMount RunFlags {} = True
+disabledDockerClean :: Shell.ParsedShell -> Bool
+disabledDockerClean args
+  | removesScript || keepsPackages = True
+  | otherwise = False
+  where
+    removesScript =
+      any
+        (Shell.cmdHasArgs "rm" ["/etc/apt/apt.conf.d/docker-clean"])
+        (Shell.presentCommands args)
+    keepsPackages =
+      any
+        ( Shell.cmdHasArgs
+            "echo"
+            ["\'Binary::apt::APT::Keep-Downloaded-Packages \"true\";\'"]
+        )
+        (Shell.presentCommands args)
+
+hasCacheDirectory :: Text.Text -> RunFlags -> Bool
+hasCacheDirectory dir RunFlags { mount } =
+  not ( null $ Set.filter (isCacheMount dir) mount)
+
+isCacheMount :: Text.Text -> RunMount -> Bool
+isCacheMount dir (CacheMount CacheOpts {cTarget = TargetPath {unTargetPath = t}}) = dir `Text.isPrefixOf` t
+isCacheMount _ _ = False
 
 -- | Even though dockerfiles without a FROM are not valid, we still want to provide some feedback for this rule
 -- so we pretend there is a base image at the start of the file if there is none
