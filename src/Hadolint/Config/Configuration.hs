hadolint/hadolint

commit bf7e48ea735db423a349fb2d7a83fe7a77091cc2

Overhaul: Reorganize tests, redesign configuration (#721)
* Overhaul: Reorganize tests, redesign configuration

config: redesign configuration handling

- Redesign configuration handling to have a clear order of precedence
  default < environment < config file < comandline options
- Implement more configuration options as environment variables and
  as options in the config files
- Define a clear default configuration
- Move configuration code into its own submodule and keep app/Main.hs
  clear of it
- Make configuration parsing and handling testable
- Add sensible locations to search for config files.
- Reorganize code and optimize for cleaner imports

tests: Use Hspec discovery to auto-discover tests

- Replace boilerplate code with hspec discovery.
  This also enables the use of Hspec hooks.
- Rename test submodules to conform with Hspecs expectations
- Make all fields in the config optional thus enabling proper priority
  based override behaviour.
- Add tests for config parsing and configuration handling
- Add tests for output formats
- Add tests for commandline argument parsing

other:

- Version bump language-docker 10.1.2 --> 10.2.0
- Verbose output now shows the whole running configuration
- Convert custom read functions for label types and sevrities to generic
  instance of `Read`.

fixes: #716
fixes: #570

* Overhaul: Replace unsafe and unnecessary code

- replace `fromJust` with pattern matching or proper functions like
  `fromMaybe`
- give descriptive names to functions with confusing or placeholder
  names
- replace custom instances of `Read` with reader functions
- replace custom instances of `Show` with instances of `Pretty`
- replace `<>>` operator with `<|>` from `Control.Applicative`

* Overhaul: Flatten config data structure

- Merge the different nested configuration data structures
- Simplify constructor usage in test suite

src/Hadolint/Config/Configuration.hs
@@ -1,0 +1,229 @@
+module Hadolint.Config.Configuration
+  ( Configuration (..),
+    PartialConfiguration (..),
+
+    applyPartialConfiguration,
+  )
+where
+
+import Data.Maybe (fromMaybe)
+import Control.Applicative
+import Data.Coerce (coerce)
+import Data.Default
+import Data.Text (Text)
+import Data.YAML ((.:?), (.!=))
+import GHC.Generics (Generic)
+import Hadolint.Formatter.Format (OutputFormat (..))
+import Hadolint.Rule (RuleCode (..), DLSeverity (..), LabelSchema)
+import Language.Docker
+import Prettyprinter
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import qualified Data.YAML as Yaml
+
+
+data Configuration =
+  Configuration
+    { noFail :: Bool,
+      noColor :: Bool,
+      verbose :: Bool,
+      format :: OutputFormat,
+      errorRules :: [RuleCode],
+      warningRules :: [RuleCode],
+      infoRules :: [RuleCode],
+      styleRules :: [RuleCode],
+      ignoreRules :: [RuleCode],
+      allowedRegistries :: Set.Set Registry,
+      labelSchema :: LabelSchema,
+      strictLabels :: Bool,
+      failureThreshold :: DLSeverity
+    }
+  deriving (Eq, Show)
+
+instance Default Configuration where
+  def =
+    Configuration
+      False
+      False
+      False
+      def
+      mempty
+      mempty
+      mempty
+      mempty
+      mempty
+      mempty
+      mempty
+      False
+      def
+
+applyPartialConfiguration ::
+  Configuration -> PartialConfiguration -> Configuration
+applyPartialConfiguration config partial =
+  Configuration
+    (fromMaybe (noFail config) (partialNoFail partial))
+    (fromMaybe (noColor config) (partialNoColor partial))
+    (fromMaybe (verbose config) (partialVerbose partial))
+    (fromMaybe (format config) (partialFormat partial))
+    (errorRules config <> partialErrorRules partial)
+    (warningRules config <> partialWarningRules partial)
+    (infoRules config <> partialInfoRules partial)
+    (styleRules config <> partialStyleRules partial)
+    (ignoreRules config <> partialIgnoreRules partial)
+    (allowedRegistries config <> partialAllowedRegistries partial)
+    (labelSchema config <> partialLabelSchema partial)
+    (fromMaybe (strictLabels config) (partialStrictLabels partial))
+    (fromMaybe (failureThreshold config) (partialFailureThreshold partial))
+
+instance Pretty Configuration where
+  pretty c =
+    nest 2
+      ( vsep
+          [ "Configuration:",
+            "no fail:" <+> pretty (noFail c),
+            "no color:" <+> pretty (noColor c),
+            "output format:" <+> pretty (format c),
+            "failure threshold:" <+> pretty (failureThreshold c),
+            prettyPrintRulelist "error" (errorRules c),
+            prettyPrintRulelist "warning" (warningRules c),
+            prettyPrintRulelist "info" (infoRules c),
+            prettyPrintRulelist "style" (styleRules c),
+            prettyPrintRulelist "ignore" (ignoreRules c),
+            "strict labels:" <+> pretty (strictLabels c),
+            prettyPrintLabelSchema (labelSchema c),
+            prettyPrintRegistries (allowedRegistries c)
+          ]
+      )
+
+prettyPrintRulelist :: String -> [RuleCode] -> Doc ann
+prettyPrintRulelist name lst =
+  nest 2 (("override" <+> pretty name <> ":\n") <> prettyPrintList pretty lst)
+
+-- | This function needs to convert the set to a list because Doc ann is not
+-- ordered.
+prettyPrintRegistries :: Set.Set Registry -> Doc ann
+prettyPrintRegistries regs =
+  nest 2 ( "allowed registries:\n"
+             <> prettyPrintList
+                 (\r -> "-" <+> pretty (unRegistry r))
+                 (Set.toList regs)
+         )
+
+prettyPrintLabelSchema :: LabelSchema -> Doc ann
+prettyPrintLabelSchema ls =
+  nest 2 ( "label schema:\n"
+             <> prettyPrintList
+                  (\(n, t) -> pretty n <> ":" <+> pretty t)
+                  (Map.toList ls)
+         )
+
+-- | pretty print a list with a custom pretty printing function for each element
+prettyPrintList :: (a -> Doc ann) -> [a] -> Doc ann
+prettyPrintList _ [] = "none"
+prettyPrintList prnt lst = vsep (fmap (\i -> "-" <+> prnt i) lst)
+
+
+data PartialConfiguration =
+  PartialConfiguration
+    { partialNoFail :: Maybe Bool,
+      partialNoColor :: Maybe Bool,
+      partialVerbose :: Maybe Bool,
+      partialFormat :: Maybe OutputFormat,
+      partialErrorRules :: [RuleCode],
+      partialWarningRules :: [RuleCode],
+      partialInfoRules :: [RuleCode],
+      partialStyleRules :: [RuleCode],
+      partialIgnoreRules :: [RuleCode],
+      partialAllowedRegistries :: Set.Set Registry,
+      partialLabelSchema :: LabelSchema,
+      partialStrictLabels :: Maybe Bool,
+      partialFailureThreshold :: Maybe DLSeverity
+    }
+  deriving (Eq, Show)
+
+
+instance Semigroup PartialConfiguration where
+  PartialConfiguration a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13
+    <> PartialConfiguration b1 b2 b3 b4 b5 b6 b7 b8 b9 b10 b11 b12 b13 =
+      PartialConfiguration
+        (b1 <|> a1)
+        (b2 <|> a2)
+        (b3 <|> a3)
+        (a4 <> b4)
+        (a5 <> b5)
+        (a6 <> b6)
+        (a7 <> b7)
+        (a8 <> b8)
+        (a9 <> b9)
+        (a10 <> b10)
+        (a11 <> b11)
+        (b12 <|> a12)
+        (a13 <> b13)
+
+instance Monoid PartialConfiguration where
+  mempty =
+    PartialConfiguration
+      Nothing
+      Nothing
+      Nothing
+      mempty
+      mempty
+      mempty
+      mempty
+      mempty
+      mempty
+      mempty
+      mempty
+      Nothing
+      mempty
+
+instance Default PartialConfiguration where
+  def = mempty
+
+instance Yaml.FromYAML PartialConfiguration where
+  parseYAML = Yaml.withMap "Configuration" $ \m -> do
+    partialNoFail <- m .:? "no-fail" .!= Nothing
+    partialNoColor <- m .:? "no-color" .!= Nothing
+    partialVerbose <- m .:? "verbose" .!= Nothing
+    partialFormat <- m .:? "output-format"
+    override <- m .:? "override" .!= mempty
+    ignored <- m .:? "ignored" .!= mempty
+    trusted <- m .:? "trusted-registries" .!= mempty
+    partialLabelSchema <- m .:? "label-schema" .!= mempty
+    partialStrictLabels <- m .:? "strict-labels" .!= Nothing
+    let partialIgnoreRules = coerce (ignored :: [Text])
+        partialErrorRules = overrideErrorRules override
+        partialWarningRules = overrideWarningRules override
+        partialInfoRules = overrideInfoRules override
+        partialStyleRules = overrideStyleRules override
+        partialAllowedRegistries = Set.fromList (coerce (trusted :: [Text]))
+    partialFailureThreshold <- m .:? "failure-threshold"
+    return PartialConfiguration {..}
+
+
+data OverrideConfig = OverrideConfig
+  { overrideErrorRules :: [RuleCode],
+    overrideWarningRules :: [RuleCode],
+    overrideInfoRules :: [RuleCode],
+    overrideStyleRules :: [RuleCode]
+  }
+  deriving (Show, Eq, Generic)
+
+instance Semigroup OverrideConfig where
+  OverrideConfig a1 a2 a3 a4 <> OverrideConfig b1 b2 b3 b4 =
+    OverrideConfig (a1 <> b1) (a2 <> b2) (a3 <> b3) (a4 <> b4)
+
+instance Monoid OverrideConfig where
+  mempty = OverrideConfig mempty mempty mempty mempty
+
+instance Yaml.FromYAML OverrideConfig where
+  parseYAML = Yaml.withMap "OverrideConfig" $ \m -> do
+    err <- m .:? "error" .!= mempty
+    wrn <- m .:? "warning" .!= mempty
+    inf <- m .:? "info" .!= mempty
+    sty <- m .:? "style" .!= mempty
+    let overrideErrorRules = coerce (err :: [Text])
+        overrideWarningRules = coerce (wrn :: [Text])
+        overrideInfoRules = coerce (inf :: [Text])
+        overrideStyleRules = coerce (sty:: [Text])
+    return OverrideConfig {..}

test/Hadolint/Config/ConfigfileSpec.hs
@@ -1,0 +1,169 @@
+module Hadolint.Config.ConfigfileSpec (spec) where
+
+import Hadolint
+import Test.Hspec
+import qualified Data.ByteString.Char8 as Bytes
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import qualified Data.YAML as Yaml
+
+
+spec :: SpecWith ()
+spec =
+  describe "Config from Configfile" $ do
+    it "parse empty config file" $ do
+      let yaml = []
+          conf = parseYaml yaml
+      conf `shouldBe` Left "empty YAML stream"
+
+    it "parse `no-fail: true`" $ do
+      let yaml = ["no-fail: true"]
+          conf = parseYaml yaml
+      conf `shouldBe` Right mempty { partialNoFail = Just True }
+
+    it "parse `no-fail: false`" $ do
+      let yaml = ["no-fail: false"]
+          conf = parseYaml yaml
+      conf `shouldBe` Right mempty { partialNoFail = Just False }
+
+    it "parse `no-color: true`" $ do
+      let yaml = ["no-color: true"]
+          conf = parseYaml yaml
+      conf `shouldBe` Right mempty { partialNoColor = Just True }
+
+    it "parse `no-color: false`" $ do
+      let yaml = ["no-color: false"]
+          conf = parseYaml yaml
+      conf `shouldBe` Right mempty { partialNoColor = Just False }
+
+    it "parse `verbose: true`" $ do
+      let yaml = ["verbose: true"]
+          conf = parseYaml yaml
+      conf `shouldBe` Right mempty { partialVerbose = Just True }
+
+    it "parse `verbose: false`" $ do
+      let yaml = ["verbose: false"]
+          conf = parseYaml yaml
+      conf `shouldBe` Right mempty { partialVerbose = Just False }
+
+    it "parse `output-format: json`" $ do
+      let yaml = ["output-format: json"]
+          conf = parseYaml yaml
+      conf `shouldBe` Right mempty { partialFormat = Just Json }
+
+    it "parse `output-format: sarif`" $ do
+      let yaml = ["output-format: sarif"]
+          conf = parseYaml yaml
+      conf `shouldBe` Right mempty { partialFormat = Just Sarif }
+
+    it "parse override error rules" $ do
+      let yaml = [ "override:",
+                   "  error:",
+                   "    - DL3020",
+                   "    - DL3040",
+                   "    - SC1020"
+                 ]
+          conf = parseYaml yaml
+      conf `shouldBe` Right
+                        mempty
+                          { partialErrorRules = ["DL3020", "DL3040", "SC1020"] }
+
+    it "parse override warning rules" $ do
+      let yaml = [ "override:",
+                   "  warning:",
+                   "    - DL3020",
+                   "    - DL3040",
+                   "    - SC1020"
+                 ]
+          conf = parseYaml yaml
+      conf `shouldBe` Right
+                        mempty
+                          { partialWarningRules = ["DL3020", "DL3040", "SC1020"]
+                          }
+
+    it "parse override info rules" $ do
+      let yaml = [ "override:",
+                   "  info:",
+                   "    - DL3020",
+                   "    - DL3040",
+                   "    - SC1020"
+                 ]
+          conf = parseYaml yaml
+      conf `shouldBe` Right
+                        mempty
+                          { partialInfoRules = ["DL3020", "DL3040", "SC1020"] }
+
+    it "parse override style rules" $ do
+      let yaml = [ "override:",
+                   "  style:",
+                   "    - DL3020",
+                   "    - DL3040",
+                   "    - SC1020"
+                 ]
+          conf = parseYaml yaml
+      conf `shouldBe` Right
+                        mempty
+                          { partialStyleRules = ["DL3020", "DL3040", "SC1020"] }
+
+    it "parse ignored rules" $ do
+      let yaml = [ "ignored:",
+                   "  - DL3020",
+                   "  - DL3040",
+                   "  - SC1020"
+                 ]
+          conf = parseYaml yaml
+      conf `shouldBe` Right
+                        mempty
+                          { partialIgnoreRules = ["DL3020", "DL3040", "SC1020"] }
+
+    it "parse trusted registries" $ do
+      let yaml = [ "trusted-registries:",
+                   "  - foobar.com",
+                   "  - barfoo.com"
+                 ]
+          conf = parseYaml yaml
+      conf `shouldBe` Right
+                        mempty
+                          { partialAllowedRegistries =
+                              Set.fromList ["foobar.com", "barfoo.com"]
+                          }
+
+    it "parse label schema" $ do
+      let yaml = [ "label-schema:",
+                   "  foo: email",
+                   "  bar: text"
+                 ]
+          conf = parseYaml yaml
+      conf `shouldBe` Right
+                        mempty
+                          { partialLabelSchema =
+                              Map.fromList [("foo", Email), ("bar", RawText)]
+                          }
+
+    it "parse strict-labels: true" $ do
+      let yaml = [ "strict-labels: true" ]
+          conf = parseYaml yaml
+      conf `shouldBe` Right mempty { partialStrictLabels = Just True }
+
+    it "parse strict-labels: false" $ do
+      let yaml = [ "strict-labels: false" ]
+          conf = parseYaml yaml
+      conf `shouldBe` Right mempty { partialStrictLabels = Just False }
+
+    it "parse `failure-threshold: warning`" $ do
+      let yaml = ["failure-threshold: warning"]
+          conf = parseYaml yaml
+      conf `shouldBe` Right mempty { partialFailureThreshold = Just DLWarningC }
+
+    it "parse `failure-threshold: style`" $ do
+      let yaml = ["failure-threshold: style"]
+          conf = parseYaml yaml
+      conf `shouldBe` Right mempty { partialFailureThreshold = Just DLStyleC }
+
+-- Helper functions for parsing config files
+
+parseYaml :: [Bytes.ByteString] -> Either String PartialConfiguration
+parseYaml ls =
+  case (Yaml.decode1Strict . Bytes.unlines) ls of
+    Left (_, err) -> Left err
+    Right res -> Right res

commit e6a72c872ce0364bc617924b67b4b2e26d3f7ea7

config: fix config file key `trustedRegistries`
Fix regression introduced by bf7e48e where the key for configuring the
set of trusted registries had changed.

fixes:  #733

src/Hadolint/Config/Configuration.hs
@@ -188,7 +188,7 @@ instance Yaml.FromYAML PartialConfiguration where
     partialFormat <- m .:? "output-format"
     override <- m .:? "override" .!= mempty
     ignored <- m .:? "ignored" .!= mempty
-    trusted <- m .:? "trusted-registries" .!= mempty
+    trusted <- m .:? "trustedRegistries" .!= mempty
     partialLabelSchema <- m .:? "label-schema" .!= mempty
     partialStrictLabels <- m .:? "strict-labels" .!= Nothing
     let partialIgnoreRules = coerce (ignored :: [Text])

test/Hadolint/Config/ConfigfileSpec.hs
@@ -117,7 +117,7 @@ spec =
                           { partialIgnoreRules = ["DL3020", "DL3040", "SC1020"] }
 
     it "parse trusted registries" $ do
-      let yaml = [ "trusted-registries:",
+      let yaml = [ "trustedRegistries:",
                    "  - foobar.com",
                    "  - barfoo.com"
                  ]