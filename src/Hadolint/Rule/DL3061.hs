6e1410f79ad685333b961dce4ccdc8991361c84c
Moritz Röhrich <moritz@ildefons.de>
2022-01-11 14:42:02 +0100

    DL3061: Check for instruction order
    Not all dockerfiles are valid just because all instructions in the
    dockerfile are individually valid. For example, a `RUN` instruction may
    not preceed the first `FROM` instruction and so on. The new rule DL3061
    checks that the order of the instructions is not completely bogus. That
    means the Dockefile must begin with a comment, an `ARG` or a `FROM`
    instruction.
    
    - Add new rule DL3061 to check for instruction order
    - Add tests for rule DL3061
    - Fix regression tests wrt. rule DL3061
    
    fixes: #737

---
+++ src/Hadolint/Rule/DL3061.hs
@@ -0,0 +1,21 @@
+module Hadolint.Rule.DL3061 (rule) where
+
+import Hadolint.Rule
+import Language.Docker.Syntax
+
+
+rule :: Rule args
+rule = customRule check (emptyState False)
+  where
+    code = "DL3061"
+    severity = DLErrorC
+    message = "Invalid instruction order. Dockerfile must begin with `FROM`,\
+              \ `ARG` or comment."
+
+    check _ st (From _) = st |> replaceWith True
+    check _ st (Comment _) = st
+    check _ st (Arg _ _) = st
+    check line st _
+      | state st = st
+      | otherwise = st |> addFail CheckFailure {..}
+{-# INLINEABLE rule #-}
---
+++ test/Hadolint/Rule/DL3061Spec.hs
@@ -0,0 +1,27 @@
+module Hadolint.Rule.DL3061Spec (spec) where
+
+import Data.Default
+import Helpers
+import Test.Hspec
+
+
+spec :: SpecWith ()
+spec = do
+  let ?config = def
+
+  describe "DL3061 - Invalid instruction order. Dockerfile must begin with\
+           \ `FROM`, `ARG` or comment." $ do
+    it "warn: label before from" $ do
+      ruleCatches "DL3061" "LABEL foo=bar\nFROM foo"
+      onBuildRuleCatches "DL3061" "LABEL foo=bar\nFROM foo"
+
+    it "don't warn: from before label" $ do
+      ruleCatchesNot "DL3061" "FROM foo\nLABEL foo=bar"
+      onBuildRuleCatchesNot "DL3061" "FROM foo\nLABEL foo=bar"
+
+    it "don't warn: ARG then FROM then LABEL" $ do
+      ruleCatchesNot "DL3061" "ARG A=B\nFROM foo\nLABEL foo=bar"
+      onBuildRuleCatchesNot "DL3061" "ARG A=B\nFROM foo\nLABEL foo=bar"
+
+    it "don't warn: FROM then ARG then RUN" $ do
+      ruleCatchesNot "DL3061" "FROM foo\nARG A=B\nRUN echo bla"
