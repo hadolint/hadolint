2f1ab2c56146685d28094836d1b0156976fcf19a
Moritz Röhrich <moritz@ildefons.de>
2022-03-18 09:57:40 +0100

    DL3061: fix false positive on pragmas (#797)
    - Fix false positive error DL3061 when encountering a pragma before a
      `FROM` instruction.
    
    In particular `# syntax = docker/dockerfile...` but also `# shell = ...`
    pragmas can appear before the first `FROM` instruction without error.
    
    fixes: #795

--- test/Hadolint/Rule/DL3061Spec.hs
+++ test/Hadolint/Rule/DL3061Spec.hs
@@ -23,5 +23,11 @@ spec = do
       ruleCatchesNot "DL3061" "ARG A=B\nFROM foo\nLABEL foo=bar"
       onBuildRuleCatchesNot "DL3061" "ARG A=B\nFROM foo\nLABEL foo=bar"
 
+    it "don't warn: pragma before FROM" $ do
+      ruleCatchesNot "DL3061" "# syntax = docker/dockerfile:1.0-experimental\n\
+                              \FROM node:16-alpine3.13"
+      onBuildRuleCatchesNot "DL3061" "# syntax = docker/dockerfile:1.0-experimental\n\
+                                     \FROM node:16-alpine3.13"
+
     it "don't warn: FROM then ARG then RUN" $ do
       ruleCatchesNot "DL3061" "FROM foo\nARG A=B\nRUN echo bla"
