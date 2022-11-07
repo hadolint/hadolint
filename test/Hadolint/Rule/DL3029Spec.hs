c8e2f1d4a9d388d9b3fcff2c7a3322864c1eebd0
Maxime Brunet <max@brnt.mx>
2022-11-07 02:09:56 -0800

    fix(DL3029): allow BUILDPLATFORM and TARGETPLATFORM variables (#867)

--- test/Hadolint/Rule/DL3029Spec.hs
+++ test/Hadolint/Rule/DL3029Spec.hs
@@ -12,4 +12,9 @@ spec = do
   describe "DL3029 - Do not use --platform flag with FROM." $ do
     it "explicit platform flag" $ ruleCatches "DL3029" "FROM --platform=linux debian:jessie"
     it "no platform flag" $ ruleCatchesNot "DL3029" "FROM debian:jessie"
-    it "allow platform $BUILDPLATFORM flag" $ ruleCatchesNot "DL3029" "FROM --platform=$BUILDPLATFORM debian:jessie"
+    it "allows platform $BUILDPLATFORM flag" $ ruleCatchesNot "DL3029" "FROM --platform=$BUILDPLATFORM debian:jessie"
+    it "allows platform \"$BUILDPLATFORM\" flag" $ ruleCatchesNot "DL3029" "FROM --platform=\"$BUILDPLATFORM\" debian:jessie"
+    it "allows platform ${BUILDPLATFORM} flag" $ ruleCatchesNot "DL3029" "FROM --platform=${BUILDPLATFORM} debian:jessie"
+    it "allows platform ${BUILDPLATFORM:-} flag" $ ruleCatchesNot "DL3029" "FROM --platform=${BUILDPLATFORM:-} debian:jessie"
+    it "allows platform \"${BUILDPLATFORM:-}\" flag" $ ruleCatchesNot "DL3029" "FROM --platform=\"${BUILDPLATFORM:-}\" debian:jessie"
+    it "allows platform $TARGETPLATFORM flag" $ ruleCatchesNot "DL3029" "FROM --platform=$TARGETPLATFORM debian:jessie"
