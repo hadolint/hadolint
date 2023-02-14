a0742bf46e9a43ca6b0cf298cc9aac0bd2fa63ad
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-02-14 01:27:08 -0500

    spelling: without
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- test/Hadolint/Rule/DL3033Spec.hs
+++ test/Hadolint/Rule/DL3033Spec.hs
@@ -10,7 +10,7 @@ spec = do
   let ?config = def
 
   describe "DL3033 - Specify version with `yum install -y <package>-<version>`" $ do
-    it "not ok wihout yum version pinning" $ do
+    it "not ok without yum version pinning" $ do
       ruleCatches "DL3033" "RUN yum install -y tomcat && yum clean all"
       onBuildRuleCatches "DL3033" "RUN yum install -y tomcat && yum clean all"
     it "ok with yum version pinning" $ do
@@ -18,7 +18,7 @@ spec = do
       ruleCatchesNot "DL3033" "RUN bash -c `# not even a yum command`"
       onBuildRuleCatchesNot "DL3033" "RUN yum install -y tomcat-9.2 && yum clean all"
       onBuildRuleCatchesNot "DL3033" "RUN bash -c `# not even a yum command`"
-    it "not ok wihout yum version pinning - modules" $ do
+    it "not ok without yum version pinning - modules" $ do
       ruleCatches "DL3033" "RUN yum module install -y tomcat && yum clean all"
       onBuildRuleCatches "DL3033" "RUN yum module install -y tomcat && yum clean all"
     it "ok with yum version pinning - modules" $ do
