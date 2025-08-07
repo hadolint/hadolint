dbb7be0737b2d170331aa0d679d5643f56416001
Tanmay Pereira Naik <tanmay.naik@smarsh.com>
2025-08-07 20:32:40 +0000

    fix: Fix build issues

--- src/Hadolint/Rule/DL3062.hs
+++ src/Hadolint/Rule/DL3062.hs
@@ -1,8 +1,12 @@
-module Hadolint.Rule.DL3062 (dl3062) where
+module Hadolint.Rule.DL3062 (rule) where
 
 import Hadolint.Rule
 import qualified Hadolint.Shell as Shell
 import Language.Docker.Syntax
+
+rule :: Rule Shell.ParsedShell
+rule = dl3062 <> onbuild dl3062
+{-# INLINEABLE rule #-}
 
 -- | DL3062: Install only essential dependencies. Instead of `npm ci/install` use `npm --production ci/install`
 dl3062 :: Rule Shell.ParsedShell
@@ -15,18 +19,18 @@ dl3062 = simpleRule code severity message check
 
     check (Run (RunArgs args _)) = foldArguments (Shell.noCommands missingProductionFlag) args
     check _ = True
+{-# INLINEABLE dl3062 #-}
 
 missingProductionFlag :: Shell.Command -> Bool
 missingProductionFlag cmd =
   (isNpmInstall cmd || isNpmCi cmd) && not (hasProductionFlag cmd)
 
 isNpmInstall :: Shell.Command -> Bool
-isNpmInstall cmd = Shell.cmdHasArgs cmd "npm" ["install"]
+isNpmInstall = Shell.cmdHasArgs "npm" ["install"]
 
 isNpmCi :: Shell.Command -> Bool
-isNpmCi cmd = Shell.cmdHasArgs cmd "npm" ["ci"]
+isNpmCi = Shell.cmdHasArgs "npm" ["ci"]
 
 hasProductionFlag :: Shell.Command -> Bool
 hasProductionFlag cmd =
-  Shell.cmdHasArgs cmd "npm" ["--production"] ||
-  Shell.cmdHasArgs cmd "npm" ["--omit=dev"]
+  Shell.hasFlag "--production" cmd || Shell.hasFlag "--omit=dev" cmd
