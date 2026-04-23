583eb4d3993286a7170fe899c7c3784975b56fa1
Moritz Röhrich <moritz@ildefons.de>
2026-04-23 11:17:07 +0200

    Revive the Hadolint online
    1) Update Hadolint to the latest release
    
    Replace the outdated transpiled JavaScript version of Hadolint with a
    recent version of Hadolint compiled to a WebAssembly artifact.
    Add some glue code for loading a WebAssembly runtime and for moving data
    between the HTML document DOM and the process in the WebAssembly
    runtime.
    
    2) Improved quality of life
    
    Make the editor trigger linting automatically after changes to the
    document.
    Change the stylesheet to reflect linter diagnostics of different
    severities with different colors.
    Show the rule code and the severity in the diagnostic message.
    
    3) Modernize the design
    
    Align the web elements in the middle of the page.
    Add a favicon.ico.
    
    4) Add GitHub Actions workflow
    Add workflow for automatically building and deploying the Hadolint
    online version.
    The workflow triggers automatically on new releases or by manual
    request.
    
    related-to: hadolint/hadolint#838
    related-to: hadolint/hadolint#980
    related-to: hadolint/hadolint#1017
    related-to: hadolint/hadolint#1049
    related-to: hadolint/hadolint#1099
    
    Signed-off-by: Moritz Röhrich <moritz@ildefons.de>

---
+++ app/embedded.hs
@@ -0,0 +1,73 @@
+-- | Hadolint Embedded
+--
+-- This is a special version of Hadolint designed to be compiled to a WASM artifact
+-- and embedded in a website.
+-- It will read the content of the Dockerfile from a textarea element with id
+-- `dockerfile__code` and output the linting result as JSON string to stdout in
+-- the WASM runtime environment.
+
+import Data.Default
+import qualified Data.Sequence as Seq
+import qualified Data.Text as Text
+import GHC.Wasm.Prim
+import qualified Hadolint
+import qualified Hadolint.Formatter.Format as Format
+import qualified Language.Docker as Docker
+import Language.Docker.Parser (DockerfileError, Error)
+import Language.Docker.Syntax (Dockerfile)
+
+
+main :: IO ()
+main = error "ignore this"
+
+
+-- JavaScript imports --
+foreign import javascript unsafe "document.getElementById($1)"
+  js_document_getElementById :: JSString -> IO JSVal
+
+foreign import javascript unsafe "document.getElementById($1).value"
+  js_document_getElementValue :: JSString -> IO JSString
+
+foreign import javascript unsafe "$1.addEventListener($2, $3)"
+  js_addEventListener :: JSVal -> JSString -> JSVal -> IO ()
+
+foreign import javascript "wrapper"
+  asEventListener :: (JSVal -> IO ()) -> IO JSVal
+
+
+-- JavaScript exports --
+foreign export javascript "setup" setup :: IO ()
+setup :: IO ()
+setup = do
+  lintInput <- js_document_getElementById (toJSString "lint")
+  lintInputCallback <- asEventListener onLint
+  js_addEventListener lintInput (toJSString "click") lintInputCallback
+
+foreign export javascript "lint" lint :: IO ()
+lint :: IO ()
+lint = do
+  dftext <- js_document_getElementValue (toJSString "dockerfile__code")
+  showResult $ doLint . Docker.parseText $ jsStringToText dftext
+
+
+-- Internal functions --
+
+-- event handler - just ignores the event object
+onLint :: JSVal -> IO ()
+onLint _ = lint
+
+doLint :: Either Error Dockerfile -> Format.Result Text.Text DockerfileError
+doLint res =
+  case res of
+    Left err -> Format.Result file ( Seq.singleton err ) mempty
+    Right ast -> Format.Result file mempty ( analyze ast )
+  where
+    file = Text.pack "Dockerfile"
+    analyze ast = Seq.unstableSort ( Hadolint.analyze def ast )
+
+showResult :: Format.Result Text.Text DockerfileError -> IO ()
+showResult result = do
+  Hadolint.printResults Hadolint.Json True Nothing [ result ]
+
+jsStringToText :: JSString -> Text.Text
+jsStringToText = Text.pack . fromJSString
