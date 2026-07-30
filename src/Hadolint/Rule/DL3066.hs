e2cd8e48a789ee999a9cc0af5ff3ea61c7878e64
Moritz Röhrich <moritz@ildefons.de>
2026-07-30 20:29:26 +0200

    DL3066: Fix false positive when UID is an argument
    Fix possible false positive when the UID for the USER instruction is
    given as an argument with an ARG instruction.
    In this case, the UID will be injected build-time and Hadolint can't
    tell if it's all-numeric or not.
    It's then best not to alert.
    
    Signed-off-by: Moritz Röhrich <moritz@ildefons.de>

--- src/Hadolint/Rule/DL3066.hs
+++ src/Hadolint/Rule/DL3066.hs
@@ -1,19 +1,31 @@
 module Hadolint.Rule.DL3066 (rule) where
 
 import qualified Data.Char as Char
+import qualified Data.Set as Set
 import qualified Data.Text as Text
 import Hadolint.Rule
 import Language.Docker.Syntax
 
+
+data Acc
+  = Acc { args :: Set.Set Text.Text }
+  | Empty
+  deriving (Show)
+
+
 rule :: Rule args
-rule = simpleRule code severity message check
+rule = customRule check (emptyState Empty)
   where
     code = "DL3066"
     severity = DLInfoC
     message = "Non-numeric user-id may not be resolvable by host system"
 
-    check (User u) = Text.all Char.isDigit $ getUid u
-    check _ = True
+    check line st (User u)
+      | Text.all Char.isDigit $ getUid u = st
+      | uidIsDefinedArg (state st) u = st
+      | otherwise = st |> addFail CheckFailure {..}
+    check _ st (Arg arg _) = st |> modify (registerArg arg)
+    check _ st _ = st
 {-# INLINEABLE rule #-}
 
 getUid :: Text.Text -> Text.Text
@@ -23,3 +35,19 @@ getUid t
   where
     u [] = ""
     u (h:_) = h
+
+uidIsDefinedArg :: Acc -> Text.Text -> Bool
+uidIsDefinedArg Empty _ = False
+uidIsDefinedArg (Acc args) u = any (`varInUid` u) args
+  where
+    varInUid :: Text.Text -> Text.Text -> Bool
+    varInUid var uid =
+      ( Text.pack "${" <> var <> Text.pack "}" ) `Text.isInfixOf` uid
+        || ( Text.pack "$" <> var ) `Text.isInfixOf` uid
+
+registerArg :: Text.Text -> Acc -> Acc
+registerArg arg Empty =
+  Acc { args = Set.singleton arg }
+registerArg arg (Acc args) =
+  Acc { args = Set.insert arg args }
+
